d0fbaea9e1f1b5149b5580cdd2ebe8a25ea3daaa
Florian Reisecker <florian.reisecker@gmail.com>
2018-12-19 21:38:14 +0100

    Solve part two of day 05

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,22 +1,34 @@
+use rayon::prelude::*;
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     part_one(&input);
+    part_two(&input);
 }
 
 fn part_one(input: &str) {
-    println!("Input size is {}", input.len());
-    println!(
-        "The solution of part 1 is {}",
-        react_recursively(&input).len()
-    );
+    println!("The solution of part 1 is {}", react_recursively(&input));
 }
 
-fn react_recursively(input: &str) -> String {
+fn part_two(input: &str) {
+    let min_length = "abcdefghijklmnopqrstuvwxyz"
+        .par_chars()
+        .map(|remove_letter| {
+            let without_letter = input
+                .replace(remove_letter, "")
+                .replace(remove_letter.to_ascii_uppercase(), "");
+            react_recursively(&without_letter)
+        })
+        .min()
+        .unwrap();
+    println!("The solution of part 2 is {}", min_length);
+}
+
+fn react_recursively(input: &str) -> usize {
     let mut changes = false;
     let mut iterator = input.chars().peekable();
-    let mut output = String::new();
+    let mut output = String::with_capacity(input.len());
     loop {
         if let Some(current) = iterator.next() {
             if let Some(next) = iterator.peek() {
@@ -38,13 +50,11 @@ fn react_recursively(input: &str) -> String {
             break;
         }
     }
-    String::from(output)
+    output.len()
 }
 
 fn reaction(a: &char, b: &char) -> bool {
-    if *a == *b {
-        return false;
-    } else if a.eq_ignore_ascii_case(b) {
+    if *a != *b && a.eq_ignore_ascii_case(b) {
         return true;
     }
     false
